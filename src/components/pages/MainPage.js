f1892576a3136bb782a9e90ac8dd5e5fb765bf36
Abdulaziz <abdulazizfaactory@gmail.com>
2022-04-07 17:00:02 +0500

    seo optimisation with helmet

--- src/components/pages/MainPage.js
+++ src/components/pages/MainPage.js
@@ -1,4 +1,6 @@
 import {useState} from 'react';
+import { Helmet } from 'react-helmet';
+
 import RandomChar from "../randomChar/RandomChar";
 import CharList from "../charList/CharList";
 import CharInfo from "../charInfo/CharInfo";
@@ -13,7 +15,14 @@ const MainPage = () => {
    
     const onCharSelected = (id) => setChar(id); 
     return (
-        <>
+        <>  
+            <Helmet>
+                <meta
+                    name="description"
+                    content="Marvel information portal"
+                />
+                <title>Marvel information portal</title>
+            </Helmet>
             <ErrorBoundary>
                 <RandomChar/>
             </ErrorBoundary>
